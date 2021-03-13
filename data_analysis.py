9fb44ab4bfe6e84c4624576844a4b2bc97899a56
Akanksha Pathak <akanksha28.iitkgp@gmail.com>
2021-03-13 10:19:06 -0600

    Update data_analysis.py
    Initially, we selected UserAcceleration and RotationRate as our two inputs. Since, these inputs are given in form of x, y, z components, we first obtain their magnitude.
     Hence, this code adds magnitude of UserAcceleration and RotationRate to original csv files and removes their x, y,z components. 
     Along with this, timestamps are also added as an additional column for our ease and further analysis

--- data_analysis.py
+++ data_analysis.py
@@ -4,15 +4,18 @@ Spyder Editor
 
 This is a temporary script file.
 """
-# CODE 1: INITIAL PREPROCESSING WITH CSV FILES
+# CODE 1: INITIAL PREPROCESSING WITH CSV FILES.
+# Initially we will work with UserAcceleration and RotationRate. Since, these inputs are given in form of x,y,z components, we first obtain their magnitude.
+# Hence, this code adds magnitue of UserAcceleration and RotationRate to original csv files and removes  their x, y,z components. 
+# ALong with this, time stamps are also added as an additional column
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt 
 import seaborn as sns
 #fold_name=['dws_1','dws_2','dws_11','ups_3','ups_4','ups_12','wlk_7','wlk_8', 'wlk_15','jog_9','jog_16','sit_5','sit_13','std_6','std_14' ]
-folder_name="E:/LOCKDOWN_READING/KAGGLE_PRACTISE/A_DeviceMotion_data/"
-Fs=50
+folder_name=".../A_DeviceMotion_data/"  # enter respective path
+Fs=50 # sampling frequency
 '''
 def mag_comp(df,col):
 # compute magnitude of vector
@@ -34,118 +37,7 @@ for i in fold_name:
          tt=np.arange(0,da.shape[0])
          tt=tt/Fs
          da['time_id']=tt
-         file_name=path_name+"/sub_N_"+str(j)+".csv"
+         file_name=path_name+"/sub_N_"+str(j)+".csv" # make new csv files for further processing
          da.to_csv(file_name,index=False)
          del da
 
-#CODE 2: Find min-max duration of data in each folder
-k=1
-
-time_dur_class=[]
-fig, a = plt.subplots(5,3)
-a=a.ravel()
-min_dur=[]
-sub_count=[]
-for axs, i in zip(a,fold_name):
-   # print(axs,i)
-    cnt=0
-    path_name=folder_name+i
-    time_dur=[]
-   # print(path_name)
-    for j in range(1,25,1):
-         file_name=path_name+"/sub_N_"+str(j)+".csv"
-         #print(file_name)
-         da=pd.read_csv(file_name)
-         tt=da['time_id']
-         time_dur.append(tt.iloc[-1])   
-         if(tt.iloc[-1]<15):
-            cnt=cnt+1
-         del da, tt               
-    
-#plt.figure()
-    min_dur.append(min(time_dur))
-    sub_count.append(cnt)
-    #fig.suptitle('Horizontally stacked subplots')
-    axs.hist(np.asarray(time_dur),bins=24)
-    #axs=sns.boxplot(np.asarray(time_dur))
-    axs.set_title(i)
-    plt.show()
-    
-    if(k>12):
-       axs.set_xlabel("Duration (sec)")
-    k=k+1   
-
-'''
-'''
-num_point=da.shape[0]
-arr=np.arange(0,num_point,1)
-da['id']=arr
-
-#plt.figure()
-da.plot(kind='line', y='userAcceleration_Mag',x='id')
-plt.show()
-da.plot(kind='line', y='rotationRate_Mag',x='id')
-plt.show()
-#zz=pd.dataframe(arr,colums=['ind'])
-'''
-
-# code 3: plot time domain signal averaged over all subjects for each class
-'''
-#sub_folder_name=['dws_2','ups_4','wlk_8','jog_16','sit_13','std_14' ]
-sub_folder_name=['dws_1','ups_3','wlk_7','jog_9','sit_5','std_6' ]
-fig, a = plt.subplots(6,1)
-a=a.ravel()
-
-for  axs, i in zip(a,sub_folder_name):
-   
-    s=np.empty([24,750])
-    path_name=folder_name+i
-    
-   # print(path_name)
-    for j in range(1,25,1):
-         file_name=path_name+"/sub_N_"+str(j)+".csv"
-         
-         da=pd.read_csv(file_name)
-         dd=da['rotationRate_Mag']  #change column name here for analyzing other columns
-         dd=(dd-dd.mean())/dd.std()
-         
-         s[j-1,:]=np.asarray(dd[0:15*Fs])
-         del da, dd
-         
-    avg_class=np.mean(s,axis=0)   
-    
-    axs.plot(avg_class)  
-    axs.set_title(i)
-    plt.show()
-    del avg_class
-    '''
-# code 4: plot average spectrum of  signal   averaged over all subjects for each class
-from scipy import signal
-
-#sub_folder_name=['dws_2','ups_4','wlk_8','jog_16','sit_13','std_14' ]
-sub_folder_name=['dws_1','ups_3','wlk_7','jog_9','sit_5','std_6' ]
-fig, a = plt.subplots(6,1)
-a=a.ravel()
-
-for  axs, i in zip(a,sub_folder_name):
-   
-    s=np.empty([24,65])
-    path_name=folder_name+i
-    
-   # print(path_name)
-    for j in range(1,25,1):
-         file_name=path_name+"/sub_N_"+str(j)+".csv"
-         
-         da=pd.read_csv(file_name)
-         dd=da['userAcceleration_Mag']  #change column name here for analyzing other columns
-         dd=(dd-dd.mean())/dd.std()
-         f, Pxx_den = signal.welch(dd, Fs, nperseg=50,nfft=128)
-         s[j-1,:]=Pxx_den
-         del da, dd, Pxx_den
-         
-    avg_psd=np.mean(s,axis=0)   
-    
-    axs.plot(f,avg_psd)  
-    axs.set_title(i)
-    plt.show()
-    del avg_psd
